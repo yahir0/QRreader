9b112072b58db3ca57b11fdb7f31386408392a2e
yahir0 <yahiro.s@me.com>
2020-10-20 23:41:36 +0900

    ・SaveActivity.class追加 ・save_activity.xml追加 ・menu.xml追加 ・Saveボタン実装 ・SharedPreferences実装 ・onCreateでのカメラの起動をコメントアウト

--- app/src/main/java/com/example/qrreader/MainActivity.java
+++ app/src/main/java/com/example/qrreader/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.qrreader;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,12 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,11 +36,12 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
         Button button2 = findViewById(R.id.button2);
         Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
         final TextView textView = findViewById(R.id.textView2);
 
 
 
-        new IntentIntegrator(MainActivity.this).setBeepEnabled(false).setCaptureActivity(MyCaptureActivity.class).setPrompt(getString(R.string.CameraMessage)).initiateScan();
+        //new IntentIntegrator(MainActivity.this).setBeepEnabled(false).setCaptureActivity(MyCaptureActivity.class).setPrompt(getString(R.string.CameraMessage)).initiateScan();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +69,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 textView.setText(R.string.message);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences Save = getSharedPreferences("SaveList",MODE_PRIVATE);
+                SharedPreferences.Editor editor = Save.edit();
+                editor.putString("input", (String) textView.getText());
+                editor.apply();
             }
         });
 
@@ -104,5 +120,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_item1:
+                Intent intent = new Intent(getApplication(),SaveActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item2:
+                setContentView(R.layout.activity_main);
+                break;
+        }
+        return true;
+    }
 }
---
+++ app/src/main/java/com/example/qrreader/SaveActivity.java
@@ -0,0 +1,38 @@
+package com.example.qrreader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SaveActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.save_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_item1:
+                break;
+            case R.id.menu_item2:
+                finish();
+                break;
+        }
+        return true;
+    }
+}
